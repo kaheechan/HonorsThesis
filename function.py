5bdd70e1937474dec491eee0819cf419f4a50b20
laraibraheem1 <148520856+laraibraheem1@users.noreply.github.com>
2023-10-31 17:55:28 -0700

    Add files via upload

--- function.py
+++ function.py
@@ -1,118 +1,76 @@
+
 import pandas as pd
-import yfinance as yf
 import sqlite3 as sq
 from dataclasses import dataclass
 from datetime import datetime
+file_path = 'C:/Users/larai/OneDrive/Desktop/ARE 107/AMZN_Simple.xlsx'
+df = pd.read_excel(file_path)
 
-class Ingestion: # Focus on SPX
-    def __init__(self):
-        self.close_data = self.close_data()
+# SMA Indicator
+def golden_cross_signal(self):
+    SMA50= self.MainDF['SMA50']
+    SMA200= self.MainDF['SMA200']
+    GoldenCrossSignal= SMA50>SMA200
+    return GoldenCrossSignal
 
-    def stock_data(self): # Print all the Data of the Stock Ticker
-        StockData = yf.download('^GSPC', start='2020-01-01', end=datetime.now().strftime('%Y-%m-%d'))
-        return StockData
+def death_cross_signal(self):
+    SMA50 = self.MainDF['SMA50']
+    SMA200 = self.MainDF['SMA200']
+    DeathCrossSignal = SMA50 < SMA200
+    return DeathCrossSignal
 
-    # Task: Get Open, High and Low Data
+#RSI Indicator
+def RSI_indicator(self, period=14): #data needed for indicator
+    close_data= self.close_data()
+    #price differences between each day's closing price
+    #and the previous day's closing price
+    price_diff= close_data.diff(1)
 
-    def close_data(self): # Get all the Stock Data for Close
-        CloseData = self.stock_data()['Close']
-        return CloseData
+    #calculate the capital gains and losses
+    gains = price_diff.where(price_diff>0,0)
+    losses= -price_diff.where(price_diff<0,0)
 
-    # Strategy One: Moving Averages
-    # Example of Moving Averages
-    def sma_10(self): # Ten Day Moving Average
-        SMA10 = self.close_data.rolling(window=10).mean()
-        return SMA10
+    #Calculate average gains and losses over 14 dats
+    #(typical measure for this strategy)
+    avg_gain= gains.rolling(window=14).mean()
+    avg_loss= losses.rolling(window=14).mean()
 
-    def sma_20(self):
-        SMA20 = self.close_data.rolling(window=20).mean()
-        return SMA20
+    #Calculate relative strength (RS)
+    rs= avg_gain/avg_loss
 
-    # Task: Do SMAFifty, SMAOneHundred, SMATwoHundred
-    def sma_50(self):
-        SMA50 = self.close_data.rolling(window=20).mean()
-        return SMA50
-
-    def sma_100(self):
-        SMA100 = self.close_data.rolling(window=100).mean()
-        return SMA100
-
-    def sma_200(self):
-        SMA200 = self. close_data.rolling(window=100).mean()
-        return SMA200
-
-    # Strategy Two: Mean Reversion
-    def std_twenty(self):
-        STDTwenty = self.close_data.rolling(window=20).std()
-        return STDTwenty
-
-    def upper_bollinger_twenty(self):
-        UpperBollingerTwenty = self.sma_20() + self.std_twenty() * 2
-        return UpperBollingerTwenty
-
-    # Task: We need Two Bands, One Upper Band and One Lower Band. I coded the Upper Band, and you can do the Lower Band
-    def lower_bollinger_twenty(self):
-        # Your Code
-        # Note: You should subtract the sma_twenty by the std_twenty() * 2
-        pass
-
-    def ema_twelve(self):
-        TwelveDays = self.close_data.ewm(span=12, adjust=False).mean()
-        return TwelveDays
+    #calculate RSI
+    rsi=100- (100/(1+rs))
+    return rs
 
 
+def rsi_bollinger_strategy(self):
+    #calculate rsi of stock
+    rsi=self.rsi
+    #calcuate the lower bollinger band based on 20-day SMA
+    # and 2 times the 20 day SD
+    sma20= self.sma20()
+    std20=self.std_twenty()
+    upper_bollinger= sma20 + 2 * std20
+    lower_bollinger= sma20 - 2 * std20
+    return lower_bollinger
+    return upper_bollinger
 
-# Inserting Data by Using SQL Database
-@dataclass
-class Insertion:
-    ColumnName: str
-    ColumnData: pd.Series | pd.DataFrame
-    Connection: sq.Connection
+#combined bollinger bands and RSI
+def combined_strategy(self):
+    rsi= self.rsi() #calculate rsi from a scale of 0 to 100
+    upper_bollinger= self.upper_bollinger()
+    lower_bollinger= self.lower_bollinger
+    if rsi<= 30 and stock_price<= lower_bollinger:
+        strategy= "Buy"
+    elif rsi>=70 and strock_price<=upper_bollinger:
+        strategy= "Sell"
 
-    def insert_data(self):
-        self.ColumnData.to_sql(self.ColumnName, self.Connection, if_exists='replace', index=True)
-        self.Connection.commit()
+#True Strength Index Indicator
+def True_Strength_Index:
 
-    def compare_data(self):
-        Query = "SELECT * FROM MainDF WHERE \"SMA10\" > \"SMA20\""
-        FilteredData = pd.read_sql_query(Query, self.Connection)
-        return FilteredData
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if rsi <= 30:
+        strategy = True
+        else strategy = False
 
 
 
